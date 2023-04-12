6598349acf470685f7e3d77204da4556c321820e
lex0819 <lexmolnar@gmail.com>
2023-04-12 22:45:35 +0300

    add searching

--- laptop/laptop.java
+++ laptop/laptop.java
@@ -20,76 +20,69 @@ public class laptop {
             String OS,
             String color,
             String year){
-        this.vendor = vendor == null ? "" : vendor;
-        this.memory = memory == null ? "" : memory;
-        this.volume_storage = volume_storage == null ? "" : volume_storage;
-        this.kind_storage = kind_storage == null ? "" : kind_storage;
-        this.OS = OS == null ? "" : OS;
-        this.color = color == null ? "" : color;
-        this.year = year == null ? "" : year;
+        this.vendor = vendor;
+        this.memory = memory;
+        this.volume_storage = volume_storage;
+        this.kind_storage = kind_storage;
+        this.OS = OS;
+        this.color = color;
+        this.year = year;
     }
 
     public Map<String, String> getLaptop(){
         Map<String, String> laptop = new HashMap<>();
 
-        laptop.put("vendor", this.vendor);
-        laptop.put("memory", this.memory);
-        laptop.put("volume_storage", this.volume_storage);
-        laptop.put("kind_storage", this.kind_storage);
-        laptop.put("OS", this.OS);
-        laptop.put("color", this.color);
-        laptop.put("year", this.year);
+        laptop.put("vendor", this.vendor == null ? "" : this.vendor);
+        laptop.put("memory", this.memory == null ? "" : this.memory);
+        laptop.put("volume_storage", this.volume_storage == null ? "" : this.volume_storage);
+        laptop.put("kind_storage", this.kind_storage == null ? "" : this.kind_storage);
+        laptop.put("OS", this.OS == null ? "" : this.OS);
+        laptop.put("color", this.color == null ? "" : this.color);
+        laptop.put("year", this.year == null ? "" : this.year);
 
         return laptop;
     }
 
     public String getVendor() {
-        return vendor;
+        return this.vendor == null ? "" : this.vendor;
     }
-
-    public String getMemory(){
-        return this.memory;
+   public String getMemory(){
+        return this.memory == null ? "" : this.memory;
     }
     public String getVolume_storage(){
-        return this.volume_storage;
+        return this.volume_storage == null ? "" : this.volume_storage;
     }
     public String getKind_storage(){
-        return this.kind_storage;
+        return this.kind_storage == null ? "" : this.kind_storage;
     }
     public String getOS(){
-        return this.OS;
+        return this.OS == null ? "" : this.OS;
     }
     public String getColor(){
-        return this.color;
+        return this.color == null ? "" : this.color;
     }
     public String getYear(){
-        return this.year;
+        return this.year == null ? "" : this.year;
     }
 
     public void setVendor(String vendor) {
         this.vendor = vendor;
     }
-
     public void setMemory(String memory) {
         this.memory = memory;
     }
-
-    public void setVolume_storage(String volume_storage) {
+   public void setVolume_storage(String volume_storage) {
         this.volume_storage = volume_storage;
     }
-
     public void setKind_storage(String kind_storage) {
         this.kind_storage = kind_storage;
     }
-
-    public void setOS(String OS) {
+   public void setOS(String OS) {
         this.OS = OS;
     }
-
-    public void setColor(String color) {
+   public void setColor(String color) {
         this.color = color;
     }
-
     public void setYear(String year) {
         this.year = year;
     }
--- laptop/pointInput.java
+++ laptop/pointInput.java
@@ -1,48 +1,98 @@
 package laptop;
 
-import java.util.ArrayList;
+import java.util.*;
 
 public class pointInput {
-    public static ArrayList<laptop> searchLaptops(String vendor,
-                                       String memory,
-                                       String year,
-                                       ArrayList<laptop> laptops){
-        ArrayList<laptop> searchList = new ArrayList<laptop>();
+    public static Set<laptop> searchRange(String memory,
+                                   String volume_storage,
+                                   String year,
+                                   HashSet<laptop> laptops){
+        Set<laptop> filterRange = new HashSet<>();
         for(laptop item : laptops){
-            if(memory == null){
-                if(item.getVendor() == vendor && item.getYear() == year){
-                    searchList.add(item);
-                }
-            }else {
-                if(item.getVendor() == vendor && item.getMemory() == memory && item.getYear() == year){
-                    searchList.add(item);
-                }
+            if(Integer.parseInt(memory) <= Integer.parseInt(item.getMemory())
+                    && Integer.parseInt(volume_storage) <= Integer.parseInt(item.getVolume_storage())
+                    && Integer.parseInt(year) <= Integer.parseInt(item.getYear())){
+                filterRange.add(item);
             }
-
         }
-        return searchList;
+        return filterRange;
     }
+
+    public static Set<laptop> searchVendor(String vendor, HashSet<laptop> laptops){
+        Set<laptop> filterVendor = new HashSet<>();
+        for(laptop item : laptops){
+            if(Objects.equals(vendor, item.getVendor())){
+                filterVendor.add(item);
+            }
+        }
+        return filterVendor;
+    }
+    public static Set<laptop> searchOS(String OS, HashSet<laptop> laptops){
+        Set<laptop> filterOS = new HashSet<>();
+        for(laptop item : laptops){
+            if(Objects.equals(OS, item.getOS())){
+                filterOS.add(item);
+            }
+        }
+        return filterOS;
+    }
+
+    public static Set<laptop> SetIntersection(Set<laptop> s1, Set<laptop> s2){
+        Set<laptop> intersection = new HashSet<>(s1);
+        intersection.retainAll(s2);
+
+        return intersection;
+    }
+
     public static void main(String[] args){
-        laptop computer01 = new laptop("ASUS","4096", "512", "SSD", "Win11", "black", "2020");
+        laptop computer01 = new laptop("ASUS","4096", "512", "SSD", "Win11", "black", "2022");
         laptop computer02 = new laptop("HP","8192", "1024", "HDD", "Win10", "black", "2019");
         laptop computer03 = new laptop("Lenovo","8192", "512", "SSD", "Win11", "black", "2021");
-        laptop computer04 = new laptop("Lenovo",null, "512", null, "Win11", null, "2021");
+        laptop computer04 = new laptop("Lenovo","4096", "512", null, "Win11", null, "2021");
+        laptop computer05 = new laptop("MacBook","8192", "256", "SSD", "MacOS", null, "2022");
+        laptop computer06 = new laptop("MacBook","8192", "512", "SSD", "MacOS", "grey", "2022");
+        laptop computer07 = new laptop("MacBook","16384", "512", "SSD", "MacOS", "grey", "2023");
 
-        ArrayList<laptop> laptops = new ArrayList<laptop>();
+        HashSet<laptop> laptops = new HashSet<>();
         laptops.add(computer01);
         laptops.add(computer02);
         laptops.add(computer03);
         laptops.add(computer04);
-        System.out.println("All our computers: ");
+        laptops.add(computer05);
+        laptops.add(computer06);
+        laptops.add(computer07);
+        System.out.println("\nAll our computers: ");
         for (laptop item: laptops){
             System.out.println(item.getLaptop());
         }
 
-        ArrayList<laptop> searchList = new ArrayList<laptop>();
-        searchList = searchLaptops("Lenovo", null, "2021", laptops);
+        Set<laptop> filtered01 = searchVendor("MacBook", laptops);
+        System.out.println("\nSearching of computers by Vendor: ");
+        for (laptop item: filtered01){
+            System.out.println(item.getLaptop());
+        }
 
-        System.out.println("Searching of computers: ");
-        for (laptop item: searchList){
+        Set<laptop> filtered02 = searchRange("4000","0","2022", laptops);
+        System.out.println("\nSearching of computers by memory and year: ");
+        for (laptop item: filtered02){
+            System.out.println(item.getLaptop());
+        }
+
+        Set<laptop> filtered03 = searchOS("Win11", laptops);
+        System.out.println("\nSearching of computers by OS: ");
+        for (laptop item: filtered03){
+            System.out.println(item.getLaptop());
+        }
+
+        Set<laptop> filtered04 = SetIntersection(filtered01, filtered02);
+        System.out.println("\nIntersection range and vendor of computers: ");
+        for (laptop item: filtered04){
+            System.out.println(item.getLaptop());
+        }
+
+        Set<laptop> filtered05 = SetIntersection(filtered03, filtered02);
+        System.out.println("\nIntersection range and OS of computers: ");
+        for (laptop item: filtered05){
             System.out.println(item.getLaptop());
         }
     }
